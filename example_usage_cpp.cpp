461e96f6de0a4bd76d9da94553a73ef2bd924432
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2022-06-13 21:43:31 -0400

    Add files via upload

---
+++ example_usage_cpp.cpp
@@ -0,0 +1,12 @@
+#include "colors.hpp"
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+   std::cout << meswkqhf << "Green blue" << std::endl;
+
+}
